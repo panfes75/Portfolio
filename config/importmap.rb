4b9810970292aec10c7ea2741fa6d426482eb1d1
nago77777 <nago2@icloud.com>
2024-04-17 23:55:43 +0900

    ヘッダーフッターのパーシャル化

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "top/index"
+pin_all_from 'app/javascript/top', under: 'top'
