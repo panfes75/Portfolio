9a8b2e478c4c15ede821fda28c15648ab4438fea
nago77777 <nago2@icloud.com>
2024-06-03 18:01:07 +0900

    README修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "static_pages#top"
+  get "/term", to: "static_pages#term"
+  get "/privacy", to: "static_pages#privacy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
