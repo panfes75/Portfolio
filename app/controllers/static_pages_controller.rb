9814cfd45fe48793c48003c34a195a385663a16c
nago77777 <nago2@icloud.com>
2024-06-16 20:44:18 +0900

    未ログイン時の遷移

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,4 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[top]
+  skip_before_action :authenticate_user!, only: %i[top term privacy]
   def top; end
 end
